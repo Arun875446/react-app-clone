19f1ed8f5baf95fcdb419cd58a51b788dc164aae
arunkumar <arun875446@gmail.com>
2023-08-26 16:31:11 +0530

    initial

---
+++ src/components/Feed.js
@@ -0,0 +1,26 @@
+
+import Thread from "./Thread"
+
+const Feed = ({ user, setOpenPopUp, filteredThreads, getThreads, setInteractingThread }) => {
+  return (
+
+    <div className="feed">
+
+      {filteredThreads?.map(filteredThread => 
+
+      <Thread key={filteredThread.id} 
+              user={user} 
+              setOpenPopUp={setOpenPopUp} 
+              filteredThread={filteredThread}
+              getThreads={getThreads}
+              setInteractingThread={setInteractingThread} 
+              
+              />)}
+
+   </div>
+
+    
+  );
+}
+
+export default Feed;
